38130e583950bf2fae7f0135269bde8f0d049042
Ali Akbar Shirazi <ali.shirazi.dev@gmail.com>
2025-08-18 21:30:35 +0330

    feat(game): add answer checking logic

--- js/game.js
+++ js/game.js
@@ -5,11 +5,15 @@ const error = document.getElementById("error");
 const container = document.getElementById("container");
 const questionText = document.getElementById("qestion-text");
 const answerList = document.querySelectorAll(".answer-text");
+const scoreText = document.getElementById("score");
 
+const CORRECT_BONUS = 10;
 const URL = `https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple`;
 let formatedData = null;
 let questionIndex = 0;
 let correctAnswer = null;
+let score = 0;
+let isAccepted = true;
 
 const fetchData = async () => {
   try {
@@ -37,4 +41,20 @@ const showQuestion = () => {
   correctAnswer = correctAnswerIndex;
 };
 
+const checkAnswer = (index, event) => {
+  if (!isAccepted) return;
+  isAccepted = false;
+  if (correctAnswer === index) {
+    event.target.classList.add("correct");
+    score += CORRECT_BONUS;
+    scoreText.innerText = score;
+  } else {
+    event.target.classList.add("incorrect");
+    answerList[correctAnswer].classList.add("correct");
+  }
+};
+
 window.addEventListener("load", fetchData);
+answerList.forEach((button, index) => {
+  button.addEventListener("click", (event) => checkAnswer(index, event));
+});
